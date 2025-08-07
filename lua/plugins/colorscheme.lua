c95031c089a9e7b4cc7a65868a09100ca108a366
omar baig <omar.a.baig@nasa.gov>
2025-08-07 14:26:45 -0500

    added a bunch of color schemes, added a color scheme randomizer

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,12 +1,33 @@
-return {
-  -- add dracula
-  { "Mofiqul/dracula.nvim" },
+-- Define your list of themes
+local colorschemes = {
+  "dracula",
+  "dracula-soft",
+  "tokyonight",
+  "tokyonight-moon",
+  "tokyonight-night",
+  "tokyonight-storm",
+  "oxocarbon",
+  "night-owl",
+  "ayu",
+  "ayu-dark",
+  "gruvbox",
+  "alduin",
+  "atom-dark",
+  "atom-dark-256",
+}
 
-  -- Configure LazyVim to load dracula
-  {
-    "LazyVim/LazyVim",
-    opts = {
-      colorscheme = "dracula",
-    },
-  },
+-- Delay setting the colorscheme until after Lazy has loaded all plugins
+vim.schedule(function()
+  vim.cmd.colorscheme(colorschemes[math.random(#colorschemes)])
+end)
+
+return {
+  { "Mofiqul/dracula.nvim" },
+  { "folke/tokyonight.nvim" },
+  { "nyoom-engineering/oxocarbon.nvim" },
+  { "haishanh/night-owl.vim" },
+  { "Shatur/neovim-ayu" },
+  { "ellisonleao/gruvbox.nvim" },
+  { "AlessandroYorba/Alduin" },
+  { "gosukiwi/vim-atom-dark" },
 }
