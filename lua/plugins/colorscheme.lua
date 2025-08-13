b22e6536c55e759b3fe57ae69723110115c60e13
omar baig <omar.a.baig@nasa.gov>
2025-08-13 09:50:18 -0500

    set colorscheme to tokyonight-night

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,25 +1,27 @@
 -- Define your list of themes
-local colorschemes = {
-  "dracula",
-  "dracula-soft",
-  "tokyonight",
-  "tokyonight-moon",
-  "tokyonight-night",
-  "tokyonight-storm",
-  "oxocarbon",
-  "night-owl",
-  "ayu",
-  "ayu-dark",
-  "gruvbox",
-  "alduin",
-  "atom-dark",
-  "atom-dark-256",
-}
+-- local colorschemes = {
+--   "dracula",
+--   "dracula-soft",
+--   "tokyonight",
+--   "tokyonight-moon",
+--   "tokyonight-night",
+--   "tokyonight-storm",
+--   "oxocarbon",
+--   "night-owl",
+--   "ayu",
+--   "ayu-dark",
+--   "gruvbox",
+--   "alduin",
+--   "atom-dark",
+--   "atom-dark-256",
+-- }
+--
+-- -- Delay setting the colorscheme until after Lazy has loaded all plugins
+-- vim.schedule(function()
+--   vim.cmd.colorscheme(colorschemes[math.random(#colorschemes)])
+-- end)
 
--- Delay setting the colorscheme until after Lazy has loaded all plugins
-vim.schedule(function()
-  vim.cmd.colorscheme(colorschemes[math.random(#colorschemes)])
-end)
+vim.cmd.colorscheme("tokyonight-night")
 
 return {
   { "Mofiqul/dracula.nvim" },
